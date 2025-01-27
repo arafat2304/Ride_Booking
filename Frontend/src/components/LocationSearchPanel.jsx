aea99d99d1c1bbe73fe5f6de42d9439344f2888c
Malek Arafat <arafatmalek44@gmail.com>
2025-01-27 11:42:10 +0530

    working

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -3,29 +3,24 @@ import React from "react";
 const LocationSearchPanel = ()=>{
     return(
         <div>
-          <div className="flex justify-center items-center gap-4 my-4">
+          <div className="p-3 flex justify-center items-center gap-4 my-2 border-2 border-grey-100 active:border-black roudede-xl">
             <h2 className="bg-[#eee] w-12 h-8 flex justify-center items-center  text-xl  rounded-full"><i className=" ri-map-pin-fill"></i></h2>
-            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase malek wada arafat nu gar </h4>
+            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase </h4>
           </div>
 
-          <div className="flex justify-center items-center gap-4 my-4">
-            <h2 className="bg-[#eee] w-12 h-8 flex justify-center items-center  text-xl rounded-full"><i className=" ri-map-pin-fill"></i></h2>
-            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase malek wada arafat nu gar </h4>
+          <div className="p-3 flex justify-center items-center gap-4 my-2 border-2 border-grey-100 active:border-black roudede-xl">
+            <h2 className="bg-[#eee] w-12 h-8 flex justify-center items-center  text-xl  rounded-full"><i className=" ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase </h4>
           </div>
 
-          <div className="flex justify-center items-center gap-4 my-4">
+          <div className="p-3 flex justify-center items-center gap-4 my-2 border-2 border-grey-100 active:border-black roudede-xl">
             <h2 className="bg-[#eee] w-12 h-8 flex justify-center items-center  text-xl  rounded-full"><i className=" ri-map-pin-fill"></i></h2>
-            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase malek wada arafat nu gar </h4>
+            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase</h4>
           </div>
 
-          <div className="flex justify-center items-center gap-4 my-4">
+          <div className="p-3 flex justify-center items-center gap-4 my-2 border-2 border-grey-100 active:border-black roudede-xl">
             <h2 className="bg-[#eee] w-12 h-8 flex justify-center items-center  text-xl  rounded-full"><i className=" ri-map-pin-fill"></i></h2>
-            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase malek wada arafat nu gar </h4>
-          </div>
-
-          <div className="flex justify-center items-center gap-4 my-4">
-            <h2 className="bg-[#eee] w-12 h-8 flex justify-center items-center  text-xl  rounded-full"><i className=" ri-map-pin-fill"></i></h2>
-            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase malek wada arafat nu gar </h4>
+            <h4 className="font-medium">near Huseni Chok,  vadad road dakor pase  </h4>
           </div>
         </div>
     )
