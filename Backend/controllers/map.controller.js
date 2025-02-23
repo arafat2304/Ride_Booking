d8b0239f2eef8c366b07e813f7112232d0908b81
Malek Arafat <arafatmalek44@gmail.com>
2025-02-23 17:15:37 +0530

    working on backend

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -1,3 +1,4 @@
+const { model } = require('mongoose');
 const mapService = require('../services/map.services');
 const {validationResult} = require('express-validator');
 
@@ -13,4 +14,31 @@ module.exports.getCordinates = async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
+};
+
+module.exports.getDistanceTime = async (req, res) => {
+    try{
+        if(!validationResult(req).isEmpty()){
+            return res.status(400).json({errors:validationResult(req).array()})
+        }
+        const { origin, destination } = req.query;
+        const distanceTime = await mapService.getDistanceTime(origin, destination);
+        res.status(200).json(distanceTime);
+    }catch(error){
+        res.status(400).json({error:error.message});
+    }
+}
+
+module.exports.getSuggestions = async (req, res) => {
+    try{
+        if(!validationResult(req).isEmpty()){
+            return res.status(400).json({errors:validationResult(req).array()})
+        }
+        const { input } = req.query;
+        const suggestions = await mapService.getSuggestions(input);
+        res.status(200).json(suggestions);
+    }catch(error){
+        res.status(400).json({error:error.message});
+    }
+
 }
--- Backend/services/map.services.js
+++ Backend/services/map.services.js
@@ -20,3 +20,47 @@ module.exports.getAddressCoordinate = async (address) => {
         throw new Error("Invalid address");
     }
 }
+
+module.exports.getDistanceTime = async (origin, destination) => {
+    if(!origin || !destination){
+        throw new Error("Invalid origin or destination");
+    }
+    const apiKey = process.env.GOOGLE_MAP_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=${apiKey}`;
+    try{
+        const response = await axios.get(url);
+        if(response.data.status === "OK"){
+           if(response.data.rows[0].elements[0].status === "ZERO_RESULTS"){
+                throw new Error("Invalid origin or destination");
+           }   
+
+           return response.data.rows[0].elements[0];
+        }else{
+            throw new Error("Invalid origin or destination");
+        }
+    }catch(error){
+        console.log(error);
+        throw new Error("Invalid origin or destination");
+    }
+
+}
+
+module.exports.getSuggestions = async (input) => {
+
+    if(!input){
+        throw new Error("query is required");
+    }
+    const apiKey = process.env.GOOGLE_MAP_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${apiKey}`;
+    try{
+        const response = await axios.get(url);
+        if(response.data.status === "OK"){
+            return response.data.predictions;
+        }else{
+            throw new Error("unable to fetch suggestions");
+        }
+    }catch(error){
+        console.log(error);
+        throw new Error("Invalid input");
+    }
+}
