d8291714fef729d03a6f9f6ad03747cdab3de796
Malek Arafat <arafatmalek44@gmail.com>
2025-05-02 12:11:25 +0530

    working for responsenes

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export default function Start() {
 
   return(
-    <div>
-      <div className="bg-cover bg-center bg-[url(https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c5310f182519763.652f3606b64b0.jpg)] h-screen pt-8  w-full flex justify-between flex-col">
-        <img className="w-16 ml-8" src="https://www.pngall.com/wp-content/uploads/4/Uber-Logo-PNG-Free-Image.png"/>
+    <div className="lg:w-1/2 lg:mx-[300px] lg:h-[300px]">
+      <div className="bg-cover bg-center bg-[url(https://cdn.pixabay.com/photo/2017/11/01/20/50/highway-2909336_1280.jpg)] h-screen pt-8  w-full flex justify-between flex-col">
+        <img className="w-16 ml-8" />
         <div className="bg-white pb-7 py-4 px-4">
-          <h2 className="text-3xl font-bold">Get started with uber</h2>
+          <h2 className="text-3xl font-bold">Get started with US</h2>
           <Link to="/login" className=" flex justify-center item-center w-full bg-black text-white rounded py-3 mt-5" >Continue</Link>
         </div>
       </div>
