93920aa2ab3519929b8b787f14fa2790c30a9b0e
Malek Arafat <arafatmalek44@gmail.com>
2025-06-12 16:30:46 +0530

    debugging

--- Backend/midlewear/auth.midlewear.js
+++ Backend/midlewear/auth.midlewear.js
@@ -9,7 +9,7 @@ module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
 
     if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'token is not exist Unauthorized' });
     }
 
 
